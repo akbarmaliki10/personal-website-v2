29a4feff6babeef5e0373af3b567ac349de2e3a2
Akbar Maliki Haqoni Jati <akbar.maliki@ui.ac.id>
2024-01-28 17:21:10 +0700

    navbar done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import styles from './App.module.css'
+import { Navbar } from './components/navbar/navbar';
+
+function App() {
+
+  return <div className={styles.App}>
+    <Navbar />
+  </div>;
+}
+
+export default App
